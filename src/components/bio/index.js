3f533cfac6b14b982d47a97388c4f4b264062d1a
dev-bomdong <dongdh02076@gmail.com>
2023-10-13 02:13:08 +0900

    fix: tag filter 주석처리 및 Bio 영역 UI 수정

--- src/components/bio/index.js
+++ src/components/bio/index.js
@@ -10,19 +10,52 @@ function Bio({ author, language = 'ko' }) {
   return (
     <div className="bio-wrapper">
       <div className="bio">
-        <div className="bio-image-wrapper" >
-        <Image
-          style={{ width: '100px', height: '100px', borderRadius: '50%' }}
-          src={bio.thumbnail}
-          alt="thumbnail"
-        />
-        </div>
-        <div className="vertical-stack">
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <div className="bio-title">@Dong_Hee</div>
-            <IconButtonBar links={social} />
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '1.5rem' }}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="54" height="14" viewBox="0 0 54 14">
+            <g fill="none" fill-rule="evenodd" transform="translate(1 1)">
+              <circle
+                cx="6"
+                cy="6"
+                r="6"
+                fill="#FF5F56"
+                stroke="#E0443E"
+                stroke-width=".5"
+              ></circle>
+              <circle
+                cx="26"
+                cy="6"
+                r="6"
+                fill="#FFBD2E"
+                stroke="#DEA123"
+                stroke-width=".5"
+              ></circle>
+              <circle
+                cx="46"
+                cy="6"
+                r="6"
+                fill="#27C93F"
+                stroke="#1AAB29"
+                stroke-width=".5"
+              ></circle>
+            </g>
+          </svg>
+
+          <div style={{ display: 'flex', gap: '1.5rem' }}>
+            <div className="bio-image-wrapper">
+              <Image
+                style={{ width: '80px', height: '80px', borderRadius: '50%' }}
+                src={bio.thumbnail}
+                alt="thumbnail"
+              />
+            </div>
+            <div className="vertical-stack">
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <div className="bio-title">@Donghee_Kim</div>
+                <IconButtonBar links={social} />
+              </div>
+              <div className="bio-desc">Front-end developer who love recording and networking</div>
+            </div>
           </div>
-          <div className="bio-desc">Front-end developer who love recording and networking</div>
         </div>
       </div>
     </div>
